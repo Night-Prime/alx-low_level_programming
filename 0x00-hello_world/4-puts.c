81cb73e7e121fc58be35f68bb008eff424bad7f6
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-03-16 14:26:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * Description: main - displays the string to sdtout
+ * Return 0: if sucess
+ *
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
